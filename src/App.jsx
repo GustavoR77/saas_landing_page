b0e5c6b63c452bbd298ebec3386070e8206c835c
Gustavo Ribeiro <gustavo_ribeiro77@hotmail.com>
2024-12-05 01:12:58 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 const App = () => {
-    return (
-        <div><h1>
-
-        </h1></div>
-    )
-}
-export default App
+  return (
+    <div>
+      <h1 className="text 3xl font-bold underline">
+        Hello World!
+      </h1>
+    </div>
+  );
+};
+export default App;
